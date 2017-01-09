913d8796c542ebe84a600b4d0e9dd00a9dfed55a
POMAZANOV Aleksej <takinado@mail.ru>
2017-01-10 00:54:27 +0200

    1.8.12-2

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo ln -sf /home/takinado/projects/web/ /home/box/
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx reload
+python /home/box/web/test.py
