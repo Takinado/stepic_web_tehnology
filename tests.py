21752343168df1647df95e91c4d815fb56c58f09
POMAZANOV Aleksej <takinado@mail.ru>
2017-01-10 00:40:11 +0200

    1.8.12-1

---
+++ tests.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+__author__ = 'takinado'
+
+import httplib
+
+
+conn = httplib.HTTPConnection("127.0.0.1")
+conn.request("GET", "/uploads/1.jpeg")
+r1 = conn.getresponse()
+
+test = 'OK' if r1.status == 200 else 'ERROR'
+print ('uploads ' + str(r1.status) + ' ' + test)
+
+data1 = r1.read()
+
+conn.request("GET", "/img/1.jpeg")
+r2 = conn.getresponse()
+
+test = 'OK' if r2.status == 200 else 'ERROR'
+print ('public ' + str(r2.status) + ' ' + test)
+
+data2 = r2.read()
+
+conn.request("GET", "/question/123")
+r3 = conn.getresponse()
+
+test = 'OK' if r3.status == 404 else 'ERROR'
+print ('w/o extention ' + str(r3.status) + ' ' + test)
+
+data3 = r3.read()
+
+conn.close()
