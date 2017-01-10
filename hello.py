db95fd428e738a4be6bb9205612f7daf6400ee48
POMAZANOV Aleksej <takinado@mail.ru>
2017-01-11 00:04:35 +0200

    1.9.12-2

--- hello.py
+++ hello.py
@@ -6,8 +6,7 @@ def app(environ, start_response):
     # data = 'Hello, World!\n'
     status = '200 OK'
     response_headers = [
-        ('Content-type','text/plain'),
-        # ('Content-Length', str(len(data)))
+        ('Content-type','text/plain')
     ]
     start_response(status, response_headers)
     return [str(i) + "\n" for i in environ['QUERY_STRING'].split("&")]
--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/hello.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-python /home/box/web/tests.py
+#python /home/box/web/tests.py
 
-#sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
-#sudo /etc/init.d/gunicorn restart
-gunicorn -b 0.0.0.0:8080 hello:app
+sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
+#gunicorn -b 0.0.0.0:8080 hello:app
