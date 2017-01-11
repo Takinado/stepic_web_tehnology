7691e0333c03f6bee9d8fe8860ce181ac2ccee22
POMAZANOV Aleksej <takinado@mail.ru>
2017-01-11 23:37:26 +0200

    2.1.11-3

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -8,13 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-# sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "..")))
-sys.path.append(PROJECT_ROOT)
+sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "..")))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
