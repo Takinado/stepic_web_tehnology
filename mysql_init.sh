0f80300c6ce2f65835808103db1372421ec56d12
POMAZANOV Aleksej <takinado@mail.ru>
2017-01-13 19:13:04 +0200

    2.2.10-3

--- mysql_init.sh
+++ mysql_init.sh
@@ -6,7 +6,7 @@
 
 #pymysql.install_as_MySQLdb() 
 #
-/etc/init.d/mysql start
+sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE web CHARACTER SET utf8 COLLATE utf8_general_ci;"
 mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY '1234';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON web.* TO 'box'@'localhost';"
